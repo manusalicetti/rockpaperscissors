eefff58ed695f69dfa4d7097c398a3b6f38d8e55
Manuel Vega <manu.vega@outlook.com>
2022-01-04 21:14:39 -0600

    computerPlay function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-console.log('What up Mafaka');
+let getRandom = (min = 1, max = 4) => {
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+let computerPlay = () => {
+  let number = getRandom();
+  if (number == 3) {
+    return 'scissors';
+  } else if (number == 2) {
+    return 'rock';
+  } else {
+    return 'paper';
+  }
+};
+
+let game = (computer, player) => {
+  let playerSelection = prompt('Write "paper, rock or scissors"');
+};
+
+console.log(computerPlay());
+
+console.log('Scissors' === 'scissors');
