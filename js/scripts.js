d1618b20f8648ac992100f876e2a5eeb9018f332
Manuel Vega <manu.vega@outlook.com>
2022-01-04 22:36:31 -0600

    game function

--- js/scripts.js
+++ js/scripts.js
@@ -15,8 +15,24 @@ let computerPlay = () => {
 
 let game = (computer, player) => {
   let playerSelection = prompt('Write "paper, rock or scissors"');
+  player = playerSelection.toLocaleLowerCase();
+  computer = computerPlay();
+
+  if ((computer == 'scissors' && player == 'scissors') || (computer == 'rock' && player == 'rock') || (computer == 'paper' && player == 'paper')) {
+    return 'Tie! Play again!';
+  } else if (computer == 'scissors' && player == 'rock') {
+    return 'You won! Rock breaks scissors';
+  } else if (computer == 'rock' && player == 'paper') {
+    return 'You won! Paper wraps rock';
+  } else if (computer == 'paper' && player == 'scissors') {
+    return 'You won! Scissors cut paper';
+  } else if (computer == 'scissors' && player == 'paper') {
+    return 'You lost! Scissors cut paper';
+  } else if (computer == 'rock' && player == 'scissors') {
+    return 'You lost! Rock breaks scissors';
+  } else if (computer == 'paper' && player == 'rock') {
+    return 'You lost! Paper wraps rock';
+  } else game();
 };
 
-console.log(computerPlay());
-
-console.log('Scissors' === 'scissors');
+console.log(game());
