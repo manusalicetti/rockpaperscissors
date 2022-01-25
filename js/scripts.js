83ddd5a54af31d3d36d6d14928b3fafc5dac80da
Manuel Vega <manu.vega@outlook.com>
2022-01-24 21:21:04 -0600

    version of the project in foundations

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+let computerScore = 0;
+let playerScore = 0;
+
 let getRandom = (min = 1, max = 4) => {
   return Math.round(Math.random() * (max - min) + min);
 };
@@ -13,26 +16,50 @@ let computerPlay = () => {
   }
 };
 
-let game = (computer, player) => {
-  let playerSelection = prompt('Write "paper, rock or scissors"');
-  player = playerSelection.toLocaleLowerCase();
-  computer = computerPlay();
-
-  if ((computer == 'scissors' && player == 'scissors') || (computer == 'rock' && player == 'rock') || (computer == 'paper' && player == 'paper')) {
+let game = (computerSelection, playerSelection) => {
+  if (
+    (computerSelection == 'scissors' && playerSelection == 'scissors') ||
+    (computerSelection == 'rock' && playerSelection == 'rock') ||
+    (computerSelection == 'paper' && playerSelection == 'paper')
+  ) {
     return 'Tie! Play again!';
-  } else if (computer == 'scissors' && player == 'rock') {
+  } else if (computerSelection == 'scissors' && playerSelection == 'rock') {
+    playerScore += 1;
     return 'You won! Rock breaks scissors';
-  } else if (computer == 'rock' && player == 'paper') {
+  } else if (computerSelection == 'rock' && playerSelection == 'paper') {
+    playerScore += 1;
     return 'You won! Paper wraps rock';
-  } else if (computer == 'paper' && player == 'scissors') {
+  } else if (computerSelection == 'paper' && playerSelection == 'scissors') {
+    playerScore += 1;
     return 'You won! Scissors cut paper';
-  } else if (computer == 'scissors' && player == 'paper') {
+  } else if (computerSelection == 'scissors' && playerSelection == 'paper') {
+    computerScore += 1;
     return 'You lost! Scissors cut paper';
-  } else if (computer == 'rock' && player == 'scissors') {
+  } else if (computerSelection == 'rock' && playerSelection == 'scissors') {
+    computerScore += 1;
     return 'You lost! Rock breaks scissors';
-  } else if (computer == 'paper' && player == 'rock') {
+  } else if (computerSelection == 'paper' && playerSelection == 'rock') {
+    computerScore += 1;
     return 'You lost! Paper wraps rock';
   } else game();
 };
 
-/* console.log(game()); */
+//*Parte nueva*/
+
+results = document.querySelector('.results');
+resultsText = document.createElement('h2');
+resultsText.style.color = '#b8b50c';
+resultsText.style.textAlign = 'center';
+
+const buttons = document.querySelectorAll('.button');
+const playerScoreParagraph = document.querySelector('.player-score');
+const computerScoreParagraph = document.querySelector('.computer-score');
+
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    resultsText.textContent = game(computerPlay(), button.dataset.game);
+    results.appendChild(resultsText);
+    playerScoreParagraph.textContent = playerScore;
+    computerScoreParagraph.textContent = computerScore;
+  });
+});
